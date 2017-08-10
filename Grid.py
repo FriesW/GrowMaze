be1f96aa156f425e130b4866eb34a85cbf05f784
FriesW <william.james.fries@gmail.com>
2017-08-10 15:06:19 -0500

    Grid Print Progress
    Basic structure of string construction complete. Node accessors are
    needed.

--- Grid.py
+++ Grid.py
@@ -21,4 +21,41 @@ class Grid:
 				nn.set_right(current)
 				current = nn
 		
-		self.left_top = current
+		self.top_left = current
+	
+	def get_pointer(self, x, y):
+		#TODO
+		return Grid_Pointer()
+	
+	def printable(self, wall_chr, node_chr, connector_chr):
+		wc = str(wall_chr)[0]
+		nc = str(node_chr)[0]
+		cc = str(connector_chr)[0]
+		nl = "\n"
+		
+		out = ""
+		current = self.top_left
+		line_start = current
+		
+		#Top wall
+		out += wc * (self.xd * 2 + 1) + nl
+		#Iterate down rows
+		for y in range(self.yd):
+			out += wc
+			#Between nodes
+			for x in range(self.xd):
+				out += nc
+				pass #TODO
+			out += wc + nl
+			#Between rows
+			if y != self.yd - 1: #Skip last row, as it doesn't exist
+				out += wc
+				for x in range(self.xd):
+					pass #TODO
+				out += wc + nl
+		#Bottom wall
+		out += wc * (self.xd * 2 + 1)
+		
+		return out
+		
+		
