f97f9e9b4c3040f1b619e07ea803303205a87e59
FriesW <william.james.fries@gmail.com>
2017-08-11 12:48:48 -0500

    Interface
    Added a very rudimentary text based interface for making mazes.

--- Maze.py
+++ Maze.py
@@ -1,11 +1,7 @@
-from Param_Utils import *
 from Grid import Grid
+from Param_Utils import sanitize_seed
 from Group_Manager import Group_Manager
 import random
-
-def hash_maze(hash):
-	l = decode(hash)
-	make_maze(l[0], l[1], l[2], l[3])
 
 def make_maze(xd, yd, sp, seed):
 	xd = int(xd)
@@ -31,7 +27,6 @@ def make_maze(xd, yd, sp, seed):
 	
 	#while gm.get_group(0).total_live_nodes() > 0:
 	while gm.get_group(0).total_all_nodes() < xd * yd:
-		print "Cycle...",
 		g = gm.get_group( random.randrange( gm.total_groups() ) )
 		#Every group will always have live nodes, until there is one group
 		sn = g.get_live_node( random.randrange( g.total_live_nodes() ) ) #source node
@@ -43,17 +38,11 @@ def make_maze(xd, yd, sp, seed):
 			cn = cns.pop(0) #candidate node
 			if not gm.node_in_group(cn):
 				sn.connect_to(cn)
-				print "action add"
 				g.add_live(cn)
 				done = True
 			elif g != gm.get_group_by(cn):
 				sn.connect_to(cn)
-				print "action combine"
 				gm.combine_groups(g, gm.get_group_by(cn))
 				done = True
 	
-	print param_format(xd, yd, sp, seed)
-	print master.printable()
-
-
-make_maze(5,5,1,"Apples")
+	return master.printable()
